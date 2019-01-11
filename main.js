b35319f4707ff2b5c1543f4f053471b809dde120
rahmo Alzahrani <abdulrahman.alzahrani@morningstar.com>
2019-01-11 15:35:04 -0600

    blockChain Added

--- main.js
+++ main.js
@@ -14,3 +14,32 @@ class block{
     return SHA256(this.index, this.previousHash, this.timestamp,JSON.stringify(this.data).toString())
     }
 }
+
+class blockChain {
+    constructor(){
+        this.chain = [blockChain.createGenesisBlock()]
+    }
+
+    static createGenesisBlock(){
+        return new block(0,Math.round(new Date().getTime()/1000),'Mother of Nature','')
+    }
+
+    getLatestBlock(){
+        let length = this.chain.length;
+        return this.chain[length-1];
+    }
+
+    //newBlock object
+    //when adding a block the hash and the previous hash is dynamically set while the other data manually
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock) //I'm still jenny from the block ?
+    }
+}
+
+let RahmoCoin = new blockChain();
+RahmoCoin.addBlock(new block(1,Math.round(new Date().getTime()/1000),10000000));
+RahmoCoin.addBlock(new block(2,Math.round(new Date().getTime()/1000),20));
+
+console.log(JSON.stringify(RahmoCoin,null,4).toString());
